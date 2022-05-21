31f1d6a4539d50c90f78b244b8a75b71751570e3
ejolly <eshin.jolly@gmail.com>
2022-05-21 01:15:14 -0400

    Experiment showing time stamps under messages

--- imports/ui/chatui/Message.svelte
+++ imports/ui/chatui/Message.svelte
@@ -1,17 +1,25 @@
 <script>
-  export let style = "you";
-  const you = style == "you";
-  const them = style == "them";
+  export let message;
+  export let username;
+  const you = message.from === username;
+  const them = message.to === username;
+  const formattedDateTime = `${message.createdAt.toLocaleTimeString()}, ${message.createdAt.toLocaleDateString()}`;
+  const formattedTime = `${message.createdAt.toLocaleTimeString()}`;
 </script>
 
 <li class="flex" class:justify-start={them} class:justify-end={you}>
-  <div
-    class="relative max-w-xl px-4 py-2 text-white shadow rounded-xl"
-    class:bg-gray-500={them}
-    class:bg-blue-500={you}
-  >
-    <span class="block">
-        <slot></slot>
-    </span>
+  <div class="max-w-xl">
+    <div
+      class="px-4 py-2 text-white shadow rounded-xl"
+      class:bg-gray-500={them}
+      class:bg-blue-500={you}
+    >
+      <p>
+        {message.text}
+      </p>
+    </div>
   </div>
 </li>
+<div class="text-xs text-gray-500" class:text-right={you} class:text-left={them}>
+  <p>{formattedTime}</p>
+</div>
--- imports/ui/chatui/MessageList.svelte
+++ imports/ui/chatui/MessageList.svelte
@@ -3,17 +3,16 @@
   import Message from "./Message.svelte";
   import { Messages } from "/imports/db/MessagesCollection";
 
-  export let username
+  export let username;
   let loading = true;
   let messages = [];
-  
+
   const handler = Meteor.subscribe("messages");
 
   $m: {
     loading = !handler.ready();
     messages = Messages.find({}).fetch();
   }
-
 </script>
 
 <div class="relative w-full p-6 overflow-y-auto h-[40rem]">
@@ -22,11 +21,7 @@
   {:else}
     <ul class="space-y-2">
       {#each messages as message}
-        {#if message.from == username}
-          <Message style={"you"}>{message.text}</Message>
-        {:else}
-          <Message style={"them"}>{message.text}</Message>
-        {/if}
+        <Message {message} {username} />
       {/each}
     </ul>
   {/if}
